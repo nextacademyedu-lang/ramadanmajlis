771ba94f4d77833c7218625ebd4cc16ba914b5c3
nextacademyedu-lang <nextacademyedu@gmail.com>
2026-03-08 13:35:02 +0200

    يشس

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
+    minimumCacheTTL: 86400,
     remotePatterns: [
       {
         protocol: 'https',
